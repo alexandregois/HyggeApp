3d8a6a7264cad6e25c9c7a790e2fdb775b2284e7
Alexandre Gois de Oliveira <alexandregois.ti@gmail.com>
2022-06-22 20:28:34 -0300

    Tela ListaEnderecos
    Tela ListaEnderecos

--- HyggeAPP/HyggeAPP/ViewModels/ViewListaEnderecosViewModel.cs
+++ HyggeAPP/HyggeAPP/ViewModels/ViewListaEnderecosViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using SharedTools.Models.WebPortal_API;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using Xamarin.Forms;
@@ -13,6 +15,33 @@ namespace HyggeAPP.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public App _app => (Application.Current as App);
+
+        ObservableCollection<UsuarioEnderecoModel> enderecos;
+        public ObservableCollection<UsuarioEnderecoModel> Enderecos
+        {
+            get
+            {
+                if (enderecos == null)
+                {
+                    enderecos = new ObservableCollection<UsuarioEnderecoModel>();
+                }
+                return enderecos;
+            }
+            set
+            {
+                if (value != enderecos)
+                {
+                    enderecos = value;
+
+                    var handler = this.PropertyChanged;
+                    if (handler != null)
+                    {
+                        handler(this,
+                            new PropertyChangedEventArgs("Enderecos"));
+                    }
+                }
+            }
+        }
 
         public ViewListaEnderecosViewModel(INavigationService navigationService)
             : base(navigationService)
